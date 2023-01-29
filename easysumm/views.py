c3216e1161ea0147f8f87a5707ff44ff2bdc6b9c
PriyankaBhatta <priyankabhatta2323@gmail.com>
2023-01-29 21:33:24 +0545

    updated tf_idf,preprocess and summarizenow function in views.py

--- easysumm/views.py
+++ easysumm/views.py
@@ -1,63 +1,46 @@
-from django.shortcuts import render
-import numpy as np
-import pandas as pd
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-from sklearn.feature_extraction.text import TfidfVectorizer
-import io
-import PyPDF2
-from docx2python import docx2python
-from sklearn.metrics.pairwise import cosine_similarity
+#This code is a Python script for a web application. The script implements 3 functions: home, preprocess, and summarizenow.
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+import numpy as np            #used for mathematical operations and numerical computations
+import nltk                   #python library for working with human language data
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import sent_tokenize, word_tokenize #it is used to tokenize text
+from nltk.corpus import stopwords
+nltk.download('stopwords')                  #reduces noise from text
+stop_words = set(stopwords.words("english"))                     
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#The home function is a simple view that returns the home.html template.
 def home(request):
     return render(request, "home.html")
 
-def extract_text_from_pdf(file):
-    # create a pdf reader object
-    pdf_reader = PyPDF2.PdfFileReader(file)
-    # iterate over each page
-    text = ""
-    for page in range(pdf_reader.getNumPages()):
-        text += pdf_reader.getPage(page).extractText()
-    return text
+#This preprocessing step is useful to clean the input text and make it ready for further processing like summary generation.
+def preprocess(document):
+    return document
 
-def generate_summary(text, num_sentences):
-    # preprocess the text
-    processed_text = preprocess_text(text)
-    # create the tf-idf matrix
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform(processed_text.splitlines())
-    feature_names = vectorizer.get_feature_names()
-    # calculate the similarity score between each sentence and the entire text
-    sentence_scores = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
-    # sort the sentences based on their scores
-    sorted_sentences = np.argsort(sentence_scores.flatten())[::-1]
-    # return the top-n scored sentences as the summary
-    return " ".join([processed_text.splitlines()[i] for i in sorted_sentences[:num_sentences]])
+#performs tf-idf operation
+def tf_idf(documents):
+    #documents = [preprocess(document) for document in documents] # preprocess the document
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+    feature_names = tfidf_vectorizer.vocabulary_.keys()
 
+
+    tfidf_scores = zip(tfidf_matrix.nonzero()[0], tfidf_matrix.data)
+    tfidf_scores_tuples = [(index, score) for index, score in tfidf_scores]
+
+    sentences = documents[0].split('. ')
+    sentences_with_scores = [(sentence, tfidf_scores_tuples[index][1]) for index, sentence in enumerate(sentences)]
+
+    top_sentences = sorted(sentences_with_scores, key=lambda x: x[1], reverse=True)[:10]
+    summary = '. '.join([sentence[0] for sentence in top_sentences])
+    return summary
+
+
+# the summarizenow function returns the home.html template
+# with the input text and summary as context variables. 
 def summarizenow(request):
-    if request.method == 'POST':
-        input_file = request.FILES.get('input_file')
-        if input_file:
-            if input_file.content_type == 'application/pdf':
-                # handle PDF file input
-                pdf_file = PyPDF2.PdfFileReader(input_file.file)
-                text = ""
-                for page in range(pdf_file.getNumPages()):
-                    text += pdf_file.getPage(page).extractText()
-                    summary = generate_summary(text, 5)
-                    return render(request, 'summarize.html', {'summary': summary})
-            elif input_file.content_type in ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
-                # handle doc file input
-                doc_file = docx2python(input_file)
-                text = doc_file.get_text()
-                summary = generate_summary(text, 5)
-                return render(request, 'home.html', {'summary': summary})
-            else:
-                return render(request, 'home.html', {'error': 'Invalid file type. Please upload a PDF or DOC file.'})
-        else:
-            return render(request, 'home.html', {'error': 'Please provide a valid file.'})
-    else:
-        return render(request, 'home.html')
+    input_text = request.POST.get('text', '')
+    summary = tf_idf([input_text]) # Use AI algorithm to generate summary
+    return render(request, 'home.html', {'input_text': input_text, 'summary':summary})
