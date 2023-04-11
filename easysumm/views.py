86ed7bb5fe2bc1cb85086dcf129cf1f715635034
Priyanka Bhatta <priyankabhatta2323@gmail.com>
2023-04-11 21:30:59 +0545

    chnages in views.py

--- easysumm/views.py
+++ easysumm/views.py
@@ -55,73 +55,21 @@ def get_paragraphs(url):
 
 
 #this function is used to extract text from files 
-def summarize_file(file_path, length):
-    if file_path.endswith('.docx'):
-        doc = docx.Document(file_path)
-        abstract = []
-        intro = []
-        conclusion = []
-        in_summary = False
-        
-        for para in doc.paragraphs:
-            if not in_summary and re.search(r'^Abstract', para.text):
-                in_summary = True
-            elif not in_summary and re.search(r'^Introduction', para.text):
-                in_summary = length in ('medium', 'long')
-            elif not in_summary and re.search(r'^Conclusion', para.text):
-                in_summary = length == 'long'
-            
-            if in_summary:
-                if para.text:
-                    if re.search(r'^Abstract', para.text):
-                        current_section = abstract
-                    elif re.search(r'^Introduction', para.text):
-                        current_section = intro
-                    elif re.search(r'^Conclusion', para.text):
-                        current_section = conclusion
-                    else:
-                        if not re.search(r'^\s*\d+\.\s', para.text) and not re.search(r'^\s*\d+\s', para.text):
-                            current_section.append(para.text)
-        
-        if length == 'short':
-            summary = abstract
-        elif length == 'medium':
-            summary = abstract + intro
-        else:
-            summary = abstract + intro + conclusion
-            
-        return '\n'.join(summary)
-        
-    elif file_path.endswith('.pdf'):
-        with open(file_path, 'rb') as f:
-            reader = PyPDF2.PdfFileReader(f)
-            text = ''
-            for i in range(reader.getNumPages()):
-                text += reader.getPage(i).extractText()
-        
-        abstract_start = re.search(r'^Abstract', text, flags=re.MULTILINE)
-        intro_start = re.search(r'^Introduction', text, flags=re.MULTILINE)
-        conclusion_start = re.search(r'^Conclusion', text, flags=re.MULTILINE)
-        
-        if abstract_start and intro_start and conclusion_start:
-            abstract = text[abstract_start.end(): intro_start.start()]
-            intro = text[intro_start.end(): conclusion_start.start()]
-            conclusion = text[conclusion_start.end():]
-            
-            if length == 'short':
-                summary = abstract.split('\n\n')
-            elif length == 'medium':
-                summary = (abstract + intro).split('\n\n')
-            else:
-                summary = (abstract + intro + conclusion).split('\n\n')
-                
-            summary = [para for para in summary if not re.search(r'^\s*\d+\.\s', para) and not re.search(r'^\s*\d+\s', para)]
-            
-            return '\n\n'.join(summary)
-        else:
-            raise ValueError('Could not find required sections in the document.')
+def summarize_file(file):
+    file_type = file.name.split('.')[-1]
+    if file_type == 'pdf':
+        pdf_reader = PyPDF2.PdfFileReader(file)
+        input_text = ' '
+        for page_num in range(pdf_reader.getNumPages()):
+            page = pdf_reader.getPage(page_num)
+            input_text += page.extractText()
+    elif file_type == 'docx':
+        input_text = docx2txt.process(file)
     else:
-        raise ValueError('Unsupported file format.')
+        return 'Invalid file format. Only PDF and DOCX files are supported.'
+    
+    summary = summarize(input_text, summary_length = 9) #summarize the extracted text
+    return summary
 
 
 def summarizenow(request):
@@ -133,35 +81,49 @@ def summarizenow(request):
         try:
             file = request.FILES['file']
             input_text = summarize_file(file)
-        except:
-            try:
-                url = request.POST['urlInput']
-                input_text = get_paragraphs(url)
-            except:
-                input_text = request.POST['text']
-                
-        if len(input_text.strip()) > 0:
             summary_length = request.POST.get('summary_length','small')
-            
             if summary_length == 'small':
                 summary_length = 9
             elif summary_length == 'medium':
                 summary_length = 15
             else:
                 summary_length = 19
-                
             summary = summarize(input_text, summary_length)
             output_text = summary
-        else:
-            output_text = 'The file or URL doesnt have valid text to be summarized.'
+        except:
+            try:
+                url = request.POST['urlInput']
+                input_text = get_paragraphs(url)
+                summary_length = request.POST.get('summary_length','small')
+                if summary_length == 'small':
+                    summary_length = 9
+                elif summary_length == 'medium':
+                    summary_length = 15
+                else:
+                    summary_length = 19
+                summary = summarize(input_text, summary_length)
+                output_text = summary
+                
+            except:
+                input_text = request.POST['text']
+                if len(input_text.strip()) >0:
+                    summary_length = request.POST.get('summary_length', 'small')
+                    if summary_length == 'small':
+                        summary_length = 9
+                    elif summary_length == 'medium':
+                        summary_length = 15
+                    else:
+                        summary_length = 19
+                    summary = summarize(input_text, summary_length)
+                    output_text = summary
 
-    else:
-        return redirect('home.html') #redirect to home page if not a POST equest
+                else:
+                    output_text = 'The file or URL doesnt have valid text to be summarized.'
 
-    return render(request, 'home.html', {'output_text': output_text,
-                                        'input_text': input_text,
-                                        'summary': summary,
-                                        })
+    return render(request, 'home.html', {'output_text':output_text,
+                                         'input_text': input_text, 
+                                         'summary':summary,
+                                         })
 
 
     
